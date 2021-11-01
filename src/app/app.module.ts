ca46566c0a1addebca9f8be86fb229c6a205028c
SergioAldana <saaldana2@misena.edu.co>
2021-11-01 12:15:24 -0500

    chore: se crean los componentes main, menu y salir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Router, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SalirComponent } from './components/salir/salir.component';
+
+const router:Routes = [
+  {path: '', component:MainComponent},
+  {path: 'logout', component:SalirComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MenuComponent,
+    SalirComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(router)
   ],
   providers: [],
   bootstrap: [AppComponent]
